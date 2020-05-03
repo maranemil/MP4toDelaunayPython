15f79da873dc20816caa54fd855b4f7a875a0f54
Emil Maran <maran.emil@gmail.com>
2020-05-03 13:51:20 +0200

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
-rm split/*.png
-rm output/*.png
+#rm split/*.png
+#rm output/*.png
 #ffmpeg -i load/in.mp4 -vf fps=1  *out%03d.png  
 ffmpeg -i load/in.mp4 -framerate 15 -r 15  -vf fps=1 split/out%03d.png
 for i in split/*.png; do python3 scipy_delaunay.py --input $i; done
